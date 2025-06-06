799149e503ebf504bf506cdb2844f186b68468fc
sudipaws02 <sudipaws02@gmail.com>
2025-06-06 09:04:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/column-map', methods=['GET', 'POST'])
+def column_map():
+    return render_template('column-map.html')
+
+@app.route('/filter-fields', methods=['GET', 'POST'])
+def filter_fields():
+    return render_template('filter_fields.html')
+
+@app.route('/run-specs', methods=['GET', 'POST'])
+def run_specs():
+    return render_template('run-specs.html')
+
+@app.route('/review', methods=['GET', 'POST'])
+def review():
+    return render_template('review.html', billing_type="EA", filename="billing.csv",
+                           column_mappings={"ProductId": "productId", "MeterId": "meterId"},
+                           filter_fields=["productId", "location", "priceType"],
+                           threads=4, retries=2)
+
+if __name__ == '__main__':
+    app.run(debug=True)
